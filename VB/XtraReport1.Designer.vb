407725e18636bfb70d498fa9920a0f84ec23a3a5
DevExpressExampleBot <fakeEmail>
2022-04-22 18:13:39 +0400

    Source auto update [skip ci]

--- VB/XtraReport1.Designer.vb
+++ VB/XtraReport1.Designer.vb
@@ -1,154 +1,160 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace RunningSumm
-	Partial Public Class XtraReport1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary> 
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class XtraReport1
 
-		#Region "Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Dim xrSummary1 As New DevExpress.XtraReports.UI.XRSummary()
-			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
-			Me.GroupHeader1 = New DevExpress.XtraReports.UI.GroupHeaderBand()
-			Me.nwindDataSet1 = New RunningSumm.nwindDataSet()
-			Me.productsTableAdapter = New RunningSumm.nwindDataSetTableAdapters.ProductsTableAdapter()
-			Me.GroupFooter1 = New DevExpress.XtraReports.UI.GroupFooterBand()
-			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
-			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
-			Me.xrLabel3 = New DevExpress.XtraReports.UI.XRLabel()
-			Me.xrLabel4 = New DevExpress.XtraReports.UI.XRLabel()
-			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
-			' 
-			' Detail
-			' 
-			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2, Me.xrLabel1})
-			Me.Detail.Height = 33
-			Me.Detail.Name = "Detail"
-			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
-			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
-			' 
-			' GroupHeader1
-			' 
-			Me.GroupHeader1.GroupFields.AddRange(New DevExpress.XtraReports.UI.GroupField() { New DevExpress.XtraReports.UI.GroupField("CategoryID", DevExpress.XtraReports.UI.XRColumnSortOrder.Ascending)})
-			Me.GroupHeader1.Height = 25
-			Me.GroupHeader1.Name = "GroupHeader1"
-			' 
-			' nwindDataSet1
-			' 
-			Me.nwindDataSet1.DataSetName = "nwindDataSet"
-			Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' productsTableAdapter
-			' 
-			Me.productsTableAdapter.ClearBeforeFill = True
-			' 
-			' GroupFooter1
-			' 
-			Me.GroupFooter1.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel4, Me.xrLabel3})
-			Me.GroupFooter1.Height = 49
-			Me.GroupFooter1.Name = "GroupFooter1"
-			' 
-			' xrLabel1
-			' 
-			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.ProductName", "")})
-			Me.xrLabel1.Location = New System.Drawing.Point(17, 8)
-			Me.xrLabel1.Name = "xrLabel1"
-			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
-			Me.xrLabel1.Size = New System.Drawing.Size(225, 25)
-			Me.xrLabel1.StylePriority.UseTextAlignment = False
-			Me.xrLabel1.Text = "xrLabel1"
-			Me.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
-			' 
-			' xrLabel2
-			' 
-			Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitsOnOrder", "")})
-			Me.xrLabel2.Location = New System.Drawing.Point(250, 8)
-			Me.xrLabel2.Name = "xrLabel2"
-			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
-			Me.xrLabel2.Size = New System.Drawing.Size(125, 25)
-			Me.xrLabel2.StylePriority.UseTextAlignment = False
-			Me.xrLabel2.Text = "xrLabel2"
-			Me.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
-			' 
-			' xrLabel3
-			' 
-			Me.xrLabel3.BackColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(236))))), (CInt(Fix((CByte(249))))), (CInt(Fix((CByte(255))))))
-			Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitsOnOrder", "")})
-			Me.xrLabel3.Font = New System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold)
-			Me.xrLabel3.ForeColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(0))))), (CInt(Fix((CByte(128))))), (CInt(Fix((CByte(192))))))
-			Me.xrLabel3.Location = New System.Drawing.Point(250, 8)
-			Me.xrLabel3.Name = "xrLabel3"
-			Me.xrLabel3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
-			Me.xrLabel3.Size = New System.Drawing.Size(125, 25)
-			Me.xrLabel3.StylePriority.UseBackColor = False
-			Me.xrLabel3.StylePriority.UseFont = False
-			Me.xrLabel3.StylePriority.UseForeColor = False
-			Me.xrLabel3.StylePriority.UseTextAlignment = False
-			xrSummary1.Func = DevExpress.XtraReports.UI.SummaryFunc.RunningSum
-			xrSummary1.Running = DevExpress.XtraReports.UI.SummaryRunning.Report
-			Me.xrLabel3.Summary = xrSummary1
-			Me.xrLabel3.Text = "xrLabel2"
-			Me.xrLabel3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
-			' 
-			' xrLabel4
-			' 
-			Me.xrLabel4.BackColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(236))))), (CInt(Fix((CByte(249))))), (CInt(Fix((CByte(255))))))
-			Me.xrLabel4.Font = New System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold)
-			Me.xrLabel4.ForeColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(0))))), (CInt(Fix((CByte(128))))), (CInt(Fix((CByte(192))))))
-			Me.xrLabel4.Location = New System.Drawing.Point(17, 8)
-			Me.xrLabel4.Name = "xrLabel4"
-			Me.xrLabel4.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
-			Me.xrLabel4.Size = New System.Drawing.Size(225, 25)
-			Me.xrLabel4.StylePriority.UseBackColor = False
-			Me.xrLabel4.StylePriority.UseFont = False
-			Me.xrLabel4.StylePriority.UseForeColor = False
-			Me.xrLabel4.StylePriority.UseTextAlignment = False
-			Me.xrLabel4.Text = "Running Total:"
-			Me.xrLabel4.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
-			' 
-			' XtraReport1
-			' 
-			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.GroupHeader1, Me.GroupFooter1})
-			Me.DataAdapter = Me.productsTableAdapter
-			Me.DataMember = "Products"
-			Me.DataSource = Me.nwindDataSet1
-			Me.DrawGrid = False
-			Me.FilterString = "[UnitsOnOrder] <> 0"
-			Me.Version = "8.2"
-			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim xrSummary1 As DevExpress.XtraReports.UI.XRSummary = New DevExpress.XtraReports.UI.XRSummary()
+            Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+            Me.GroupHeader1 = New DevExpress.XtraReports.UI.GroupHeaderBand()
+            Me.nwindDataSet1 = New RunningSumm.nwindDataSet()
+            Me.productsTableAdapter = New RunningSumm.nwindDataSetTableAdapters.ProductsTableAdapter()
+            Me.GroupFooter1 = New DevExpress.XtraReports.UI.GroupFooterBand()
+            Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel3 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel4 = New DevExpress.XtraReports.UI.XRLabel()
+            CType((Me.nwindDataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' Detail
+            ' 
+            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.xrLabel2, Me.xrLabel1})
+            Me.Detail.Height = 33
+            Me.Detail.Name = "Detail"
+            Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' GroupHeader1
+            ' 
+            Me.GroupHeader1.GroupFields.AddRange(New DevExpress.XtraReports.UI.GroupField() {New DevExpress.XtraReports.UI.GroupField("CategoryID", DevExpress.XtraReports.UI.XRColumnSortOrder.Ascending)})
+            Me.GroupHeader1.Height = 25
+            Me.GroupHeader1.Name = "GroupHeader1"
+            ' 
+            ' nwindDataSet1
+            ' 
+            Me.nwindDataSet1.DataSetName = "nwindDataSet"
+            Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' productsTableAdapter
+            ' 
+            Me.productsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' GroupFooter1
+            ' 
+            Me.GroupFooter1.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.xrLabel4, Me.xrLabel3})
+            Me.GroupFooter1.Height = 49
+            Me.GroupFooter1.Name = "GroupFooter1"
+            ' 
+            ' xrLabel1
+            ' 
+            Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.ProductName", "")})
+            Me.xrLabel1.Location = New System.Drawing.Point(17, 8)
+            Me.xrLabel1.Name = "xrLabel1"
+            Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel1.Size = New System.Drawing.Size(225, 25)
+            Me.xrLabel1.StylePriority.UseTextAlignment = False
+            Me.xrLabel1.Text = "xrLabel1"
+            Me.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+            ' 
+            ' xrLabel2
+            ' 
+            Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitsOnOrder", "")})
+            Me.xrLabel2.Location = New System.Drawing.Point(250, 8)
+            Me.xrLabel2.Name = "xrLabel2"
+            Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel2.Size = New System.Drawing.Size(125, 25)
+            Me.xrLabel2.StylePriority.UseTextAlignment = False
+            Me.xrLabel2.Text = "xrLabel2"
+            Me.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+            ' 
+            ' xrLabel3
+            ' 
+            Me.xrLabel3.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((236)))))), (CInt(((CByte((249)))))), (CInt(((CByte((255)))))))
+            Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() {New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitsOnOrder", "")})
+            Me.xrLabel3.Font = New System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold)
+            Me.xrLabel3.ForeColor = System.Drawing.Color.FromArgb((CInt(((CByte((0)))))), (CInt(((CByte((128)))))), (CInt(((CByte((192)))))))
+            Me.xrLabel3.Location = New System.Drawing.Point(250, 8)
+            Me.xrLabel3.Name = "xrLabel3"
+            Me.xrLabel3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel3.Size = New System.Drawing.Size(125, 25)
+            Me.xrLabel3.StylePriority.UseBackColor = False
+            Me.xrLabel3.StylePriority.UseFont = False
+            Me.xrLabel3.StylePriority.UseForeColor = False
+            Me.xrLabel3.StylePriority.UseTextAlignment = False
+            xrSummary1.Func = DevExpress.XtraReports.UI.SummaryFunc.RunningSum
+            xrSummary1.Running = DevExpress.XtraReports.UI.SummaryRunning.Report
+            Me.xrLabel3.Summary = xrSummary1
+            Me.xrLabel3.Text = "xrLabel2"
+            Me.xrLabel3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+            ' 
+            ' xrLabel4
+            ' 
+            Me.xrLabel4.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((236)))))), (CInt(((CByte((249)))))), (CInt(((CByte((255)))))))
+            Me.xrLabel4.Font = New System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold)
+            Me.xrLabel4.ForeColor = System.Drawing.Color.FromArgb((CInt(((CByte((0)))))), (CInt(((CByte((128)))))), (CInt(((CByte((192)))))))
+            Me.xrLabel4.Location = New System.Drawing.Point(17, 8)
+            Me.xrLabel4.Name = "xrLabel4"
+            Me.xrLabel4.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel4.Size = New System.Drawing.Size(225, 25)
+            Me.xrLabel4.StylePriority.UseBackColor = False
+            Me.xrLabel4.StylePriority.UseFont = False
+            Me.xrLabel4.StylePriority.UseForeColor = False
+            Me.xrLabel4.StylePriority.UseTextAlignment = False
+            Me.xrLabel4.Text = "Running Total:"
+            Me.xrLabel4.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+            ' 
+            ' XtraReport1
+            ' 
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.Detail, Me.GroupHeader1, Me.GroupFooter1})
+            Me.DataAdapter = Me.productsTableAdapter
+            Me.DataMember = "Products"
+            Me.DataSource = Me.nwindDataSet1
+            Me.DrawGrid = False
+            Me.FilterString = "[UnitsOnOrder] <> 0"
+            Me.Version = "8.2"
+            CType((Me.nwindDataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
+        End Sub
 
-		Private Detail As DevExpress.XtraReports.UI.DetailBand
-		Private GroupHeader1 As DevExpress.XtraReports.UI.GroupHeaderBand
-		Private nwindDataSet1 As nwindDataSet
-		Private productsTableAdapter As RunningSumm.nwindDataSetTableAdapters.ProductsTableAdapter
-		Private GroupFooter1 As DevExpress.XtraReports.UI.GroupFooterBand
-		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
-		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
-		Private xrLabel4 As DevExpress.XtraReports.UI.XRLabel
-		Private xrLabel3 As DevExpress.XtraReports.UI.XRLabel
-	End Class
+'#End Region
+        Private Detail As DevExpress.XtraReports.UI.DetailBand
+
+        Private GroupHeader1 As DevExpress.XtraReports.UI.GroupHeaderBand
+
+        Private nwindDataSet1 As RunningSumm.nwindDataSet
+
+        Private productsTableAdapter As RunningSumm.nwindDataSetTableAdapters.ProductsTableAdapter
+
+        Private GroupFooter1 As DevExpress.XtraReports.UI.GroupFooterBand
+
+        Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+
+        Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+
+        Private xrLabel4 As DevExpress.XtraReports.UI.XRLabel
+
+        Private xrLabel3 As DevExpress.XtraReports.UI.XRLabel
+    End Class
 End Namespace
--- VB/XtraReport1.vb
+++ VB/XtraReport1.vb
@@ -1,13 +1,11 @@
-Imports Microsoft.VisualBasic
-Imports System
 ' ...
-
 Namespace RunningSumm
-	Partial Public Class XtraReport1
-		Inherits DevExpress.XtraReports.UI.XtraReport
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-	End Class
+    Public Partial Class XtraReport1
+        Inherits DevExpress.XtraReports.UI.XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
